69dc71db4e2ed05952d79c41e8c096fb89aede39
zhanba <c5e1856@gmail.com>
2020-01-07 15:00:04 +0800

    feat: 添加sockjs通信

--- src/treeDataProvider/treeDataProvider.ts
+++ src/treeDataProvider/treeDataProvider.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
+import * as SockJS from 'sockjs-client';
 
 export class UmiTaskProvider implements vscode.TreeDataProvider<UmiTask> {
   constructor(private workspaceRoot: string | undefined) {}
@@ -43,25 +44,56 @@ export class UmiTaskView {
       treeDataProvider: umiTaskProvider,
     });
 
+    let sock = new SockJS('http://localhost:3000/umiui');
+
     vscode.commands.registerCommand('umi.openui', () => {
       const panel = vscode.window.createWebviewPanel('umi', 'Umi UI', vscode.ViewColumn.One, {
         retainContextWhenHidden: true,
         enableScripts: true,
       });
+
+      panel.webview.onDidReceiveMessage(
+        message => {
+          if (message === 'initSocket') {
+            panel.webview.postMessage('sockopen');
+          } else {
+            sock.send(message);
+          }
+        },
+        undefined,
+        context.subscriptions
+      );
+
+      sock.onopen = () => {
+        panel.webview.postMessage('sockopen');
+        vscode.window.showInformationMessage('sock open');
+      };
+
+      sock.onclose = () => {
+        panel.webview.postMessage('sockclose');
+        sock = null;
+        vscode.window.showErrorMessage('sock close');
+      };
+
+      sock.onmessage = (e: any) => {
+        // const message = JSON.parse(e.data);
+        panel.webview.postMessage(e.data);
+      };
+
       const getLocalFileUrl = (localPath: string) =>
         panel.webview.asWebviewUri(vscode.Uri.file(localPath));
 
       const cssPath = getLocalFileUrl(
-        path.join(context.extensionPath, 'src', 'assets', 'dist', 'umi.dd52a6f4.css')
+        path.join(context.extensionPath, 'src', 'assets', 'dist', 'umi.ecab5baa.css')
       );
       const jsPath = getLocalFileUrl(
-        path.join(context.extensionPath, 'src', 'assets', 'dist', 'umi.c855d08e.js')
+        path.join(context.extensionPath, 'src', 'assets', 'dist', 'umi.65bcf4a5.js')
       );
       panel.webview.html = this.getWebviewContent(cssPath, jsPath);
     });
   }
 
-  getWebviewContent(cssUrl: vscode.Uri, jsUrl: vscode.Uri) {
+  getWebviewContent00(cssUrl: vscode.Uri, jsUrl: vscode.Uri) {
     return `<!DOCTYPE html>
     <html>
       <head>
@@ -75,6 +107,7 @@ export class UmiTaskView {
         <title>Umi UI</title>
         <script>
           window.routerBase = '/';
+          window.isVscodeWebview = true;
         </script>
         <script src="http://b.alicdn.com/s/polyfill.min.js?features=default,es2015,es2016,es2017,fetch,IntersectionObserver,NodeList.prototype.forEach,NodeList.prototype.@@iterator,EventSource,MutationObserver,ResizeObserver,HTMLCanvasElement.prototype.toBlob"></script>
         <script src="http://gw.alipayobjects.com/os/lib/??react/16.8.6/umd/react.production.min.js,react-dom/16.8.6/umd/react-dom.production.min.js"></script>
@@ -84,10 +117,27 @@ export class UmiTaskView {
         <script src="http://gw.alipayobjects.com/os/lib/xterm/4.1.0/lib/xterm.js"></script>
       </head>
       <body>
-        <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
         <div id="root"></div>
         <script src="${jsUrl}"></script>
+        <iframe />
       </body>
     </html>`;
+  }
+
+  getWebviewContent() {
+    return `<!DOCTYPE html>
+    <html lang="en" style="width: 100; height: 100%;">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Umi UI</title>
+      </head>
+    
+      <body style="width: 100%;height: 100%;padding: 0;margin: 0;">
+        <iframe frameborder="0" src="http://localhost:3000/" height="100%" width="100%"></iframe>
+      </body>
+    </html>
+    `;
   }
 }
